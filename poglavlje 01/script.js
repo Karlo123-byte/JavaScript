7d4c61fd2014172dfc422ed1b42dac7da2be616d
Karlo123-Byte <karlo.jozic@ekonomska.edu.ba>
2025-05-10 00:35:19 +0200

    Rijesio sam prvo poglavlje u JS-u

---
+++ poglavlje 01/script.js
@@ -0,0 +1,18 @@
+let name = "Karlo";
+let surname = "Jozic";
+
+let nameAndSurname = name+" "+surname;
+
+console.log(nameAndSurname);
+
+//dvi variable prvi broj, drugi broj, zbir zbroji rve dvi varijable
+
+let brojPrvi = 25;
+let brojDrugi = 20;
+let zbrojPrviDrugi = brojPrvi + brojDrugi;
+
+console.log(zbrojPrviDrugi)
+
+let fruit = ["Banana","Apple","Mango"];
+
+console.log(fruit);
