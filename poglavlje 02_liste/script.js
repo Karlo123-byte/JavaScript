102ae9d127ac43fe15d03681206c39cfb2056f5d
Karlo123-Byte <karlo.jozic@ekonomska.edu.ba>
2025-05-10 05:05:52 +0200

    Mali update sto se tice lista i par metoda istih.

--- poglavlje 02_liste/script.js
+++ poglavlje 02_liste/script.js
@@ -1,5 +1,17 @@
 let products = ["Kruh", "Mlijeko", "Jogurt"];
-console.log(products, products[1]);
+console.log(products);
 
 products.push("Novine"); //Vjezba dodavanje Novina preko push metode.
 console.log(products);
+
+delete products[0];
+console.log(products); //Ovdje smo obrisali kruh sa liste.
+
+let cars = ["BMW", "Mercedes", "Audi"];
+cars[0] = "Porsche"; //ovdje smo zamjenuli Porshe sa BMW-om
+console.log(cars);
+
+cars.sort();
+console.log(cars); //Ovdje smo sortirali aute po abecedi.
+
+
