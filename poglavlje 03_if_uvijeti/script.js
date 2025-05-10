92aafb3b493d1593a127c78e21d30c3c7bdc7226
Karlo123-Byte <karlo.jozic@ekonomska.edu.ba>
2025-05-10 19:57:42 +0200

    3.10 JavaScript

---
+++ poglavlje 03_if_uvijeti/script.js
@@ -0,0 +1,27 @@
+let proizvod = "kruh";
+let cijena = 99;
+
+if (cijena == 99) {
+    console.log("Kruh je skup")
+}
+
+//Vježba ako imamo varijablu kruh kroz if uslov ispisati nasli smo kruh.
+
+if (proizvod == "kruh") {
+    console.log("Našli smo kruh")
+}
+
+let automobil = "Mercedes";
+
+if (automobil == "BMW") {
+    console.log("M3");
+}
+else {
+    console.log("Nije BMW");
+}
+
+//Napraviti novi file pod nazivom vjezba.js
+//Napravite varijablu video
+//Stavite da je vrijednost varijable video
+//napravite uvijet ako je vr el video ispisati neki link od yt videa
+//ako je vr el slika ispisati link neke slike.
